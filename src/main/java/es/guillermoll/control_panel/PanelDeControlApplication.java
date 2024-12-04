cfda01ea74b87ece601bd44007a35aa4b48ac3e5
guillermoll <guillermo.jobs@hotmail.com>
2024-12-04 12:09:21 +0000

    first commit

---
+++ src/main/java/es/guillermoll/control_panel/PanelDeControlApplication.java
@@ -0,0 +1,13 @@
+package es.guillermoll.control_panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelDeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanelDeControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/guillermoll/control_panel/PanelDeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package es.guillermoll.control_panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanelDeControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
